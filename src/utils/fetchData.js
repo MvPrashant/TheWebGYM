c267ad6c9208098c7b5c59631b890d1641554753
MvPrashant <mvprashant.99@gmail.com>
2022-09-30 16:05:16 +0530

    updated2

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key': 'a570999787msh521e9ebdfd7fad9p107f22jsn1399b98fd10f',
+      'X-RapidAPI-Key': '8d83a6ad5emsh19104ace07ef82ep1310e2jsne83d2cf71faa'
       // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
     }
   };
